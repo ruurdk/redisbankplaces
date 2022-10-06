170a421798131594f2cdac610f5b446247b3a0fd
Alexandre Vasseur <avasseur@gmail.com>
2022-10-06 12:03:33 +0200

    fix JedisPool config for docker env var

--- src/main/java/com/redislabs/demos/redisbankplaces/RedisbankPlacesApplication.java
+++ src/main/java/com/redislabs/demos/redisbankplaces/RedisbankPlacesApplication.java
@@ -1,5 +1,7 @@
 package com.redislabs.demos.redisbankplaces;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,27 +13,37 @@ import redis.clients.jedis.JedisPool;
 
 @SpringBootApplication
 @Configuration
+@Slf4j
 public class RedisbankPlacesApplication {
 
-	@Bean
+/*
 	JedisConnectionFactory jedisConnectionFactory() {
 		JedisConnectionFactory factory = new JedisConnectionFactory();
 		return factory;
 	}
+*/
+
+
+	@Value("${spring.redis.host}")
+	String host;
+	@Value("${spring.redis.port}")
+	int port;
 
 	@Bean
 	JedisPool jedisPool() {
-		return new JedisPool();
+		log.info("Connecting JedisPool to Redis " + host + ":"+port);
+		return new JedisPool(host, port);
 	}
 
-	@Bean
+
+/*	@Bean
 	public RedisTemplate<String, Object> redisTemplate() {
 		final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
 		template.setConnectionFactory(jedisConnectionFactory());
 		template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
 		return template;
 	}
-
+*/
 	public static void main(String[] args) {
 		SpringApplication.run(RedisbankPlacesApplication.class, args);
 	}
--- src/main/java/com/redislabs/demos/redisbankplaces/places/DataLoader.java
+++ src/main/java/com/redislabs/demos/redisbankplaces/places/DataLoader.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
@@ -13,6 +15,7 @@ import redis.clients.jedis.Pipeline;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.InputStream;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
@@ -27,14 +30,19 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
     @Autowired
     JedisPool jedis;
 
+    @Autowired
+    ResourceLoader resourceLoader;
+
     @Override
     @SneakyThrows
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(LOAD));
+        //BufferedReader bufferedReader = new BufferedReader(new FileReader(LOAD));
+        Resource resource = resourceLoader.getResource("classpath:places.json");
+        InputStream inputStream = resource.getInputStream();
 
         // Serialise to Place Array from disk
         ObjectMapper mapper = new ObjectMapper();
-        List<Place> places = Arrays.asList(mapper.readValue(Paths.get(LOAD).toFile(), Place[].class));
+        List<Place> places = Arrays.asList(mapper.readValue(inputStream, Place[].class));
         log.info("Loading {} places.json into Redis", places.size());
 
         // Batch load into Redis
